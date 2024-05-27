fddb2c62592d8d610e85ecd0ee3b1d8f0ea877be
Elham-Mashhadi <elham.mashhadi.5@gmail.com>
2024-05-27 11:04:48 +0330

    Data

---
+++ DataType.java
@@ -0,0 +1,26 @@
+public class DataType {
+    public static void main(String[] args) {
+        String Name="Elham Mashhadi";
+        short YearOfBirth=1983;
+        byte Age=40;
+        int NationalCode=0011223344;
+        long PhoneNumber=1232524155;
+        float Grade=18.75f;
+        boolean Persian=true;
+        char Sex='F';
+
+
+        System.out.println("Name: "+Name);
+        System.out.println("Sex: "+Sex);
+        System.out.println("YearOfBirth: "+YearOfBirth);
+        System.out.println("Age: "+Age);
+        System.out.println("PhoneNumber: "+PhoneNumber);
+        System.out.println("NationalCode: "+NationalCode);
+        System.out.println("Grade: "+Grade);
+        System.out.println("Persian: "+Persian);
+
+
+    }
+}
+
+
